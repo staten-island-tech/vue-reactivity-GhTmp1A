8d90add2d8707cfcfe45f11157d734b8b326a5ff
GhTmp1A <stanbutter4@gmail.com>
2023-03-08 14:01:11 -0500

    L4 :50 in video. Review Vue documentation.

--- Vue/src/views/AboutView.vue
+++ Vue/src/views/AboutView.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="about">
     <h1>VUE PLEASE WORK</h1>
-    <h1>VUE PLEASE WORK</h1>
   </div>
 </template>
 
--- Vue/src/views/HomeView.vue
+++ Vue/src/views/HomeView.vue
@@ -1,4 +1,4 @@
-<script setup>
+<!-- <script setup>
 import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
@@ -7,3 +7,34 @@ import TheWelcome from '../components/TheWelcome.vue'
     <TheWelcome />
   </main>
 </template>
+ -->
+
+<template>
+  <div class="home">
+    <!--     <h1 v-if="graduated">{{ student }}</h1>
+    <h1 v-else>"They have not graduated"</h1>
+    <ul>
+      <li v-for="animal in animals" :key="animal">{{ animal }}</li>
+    </ul> -->
+    <button v-if="loggedIn">Log Out</button>
+    <button></button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  components: {},
+  data() {
+    return {
+      /*       student: 'Harry',
+      graduated: true,
+      animals: ['Pig', 'Horse', 'Donkey', 'Cow', 'Duck']
+      //animals.forEach((animal) => (animal.insertAdjacentHTML etc)) */
+      loggedIn: false
+    }
+  }
+}
+</script>
+
+<style scoped></style>
