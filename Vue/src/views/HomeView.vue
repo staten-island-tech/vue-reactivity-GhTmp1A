a2d6fd9ad7f5449723b04bacfadb823814bed239
GhTmp1A <stanbutter4@gmail.com>
2023-03-09 13:59:49 -0500

    L6 Computed Properties 2:21

--- Vue/src/views/HomeView.vue
+++ Vue/src/views/HomeView.vue
@@ -11,13 +11,23 @@ import TheWelcome from '../components/TheWelcome.vue'
 
 <template>
   <div class="home">
-    <!--     <h1 v-if="graduated">{{ student }}</h1>
-    <h1 v-else>"They have not graduated"</h1>
-    <ul>
-      <li v-for="animal in animals" :key="animal">{{ animal }}</li>
-    </ul> -->
-    <button v-if="loggedIn">Log Out</button>
-    <button></button>
+    <!--     <input type="text" placeholder="edit me" v-model="message" />
+    <p>{{ message }}</p>
+    <input type="checkbox" id="jack" value="Jack" v-model="checkedNames" />
+    Reflects whatever data is selected in the export data.
+    <label for="jack">Jack</label>
+    <input type="checkbox" id="john" value="John" v-model="checkedNames" />
+    <label for="john">John</label>
+    <input type="checkbox" id="mike" value="Mike" v-model="checkedNames" />
+    <label for="mike">Mike</label>
+    <br />
+    <span>Checked names: {{ checkedNames }}</span> -->
+    <select v-model="selected">
+      <option>A</option>
+      <option>B</option>
+      <option>C</option>
+    </select>
+    <span>{{ selected }}</span>
   </div>
 </template>
 
@@ -27,11 +37,35 @@ export default {
   components: {},
   data() {
     return {
-      /*       student: 'Harry',
-      graduated: true,
-      animals: ['Pig', 'Horse', 'Donkey', 'Cow', 'Duck']
-      //animals.forEach((animal) => (animal.insertAdjacentHTML etc)) */
-      loggedIn: false
+      /* message: ''
+      checkedNames: [] */
+      selected: ''
+    }
+  },
+  methods: {},
+  // components: {},
+  // data() {
+  //   return {
+  //     /*       student: 'Harry',
+  //     graduated: true,
+  //     animals: ['Pig', 'Horse', 'Donkey', 'Cow', 'Duck']
+  //     //animals.forEach((animal) => (animal.insertAdjacentHTML etc)) */
+  //     loggedIn: false
+  //   }
+  // },
+  // methods: {
+  //   authState: function () {
+  //     if (this.loggedIn === false) {
+  //       this.loggedIn = true
+  //     } else {
+  //       this.loggedIn = false //THIS CAN BE USED FOR DARK/LIGHT MODE FUNCTIONS.
+  //     }
+  //   }
+  // }
+  computed: {
+    now: function () {
+      const time = new Date()
+      return `${time.getHours}:${time.getMinutes}`
     }
   }
 }
